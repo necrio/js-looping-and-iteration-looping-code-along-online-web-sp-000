76091ad124cc753131bc1fcb07640df7a15c86bc
Keith Isham <kisham8899@gmail.com>
2020-06-17 22:45:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-// Code your solutions in this file
-for (let i = 0; i < gifts.length; i++) {
-  console.log('THank you, #{')
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return thankYouCards
+}
+
+function countdown( countdown ) {
+  while ( countdown > 0 ) {
+    console.log( countdown );
+    countdown -= 1;
+  }
+  console.log( countdown );
 }
